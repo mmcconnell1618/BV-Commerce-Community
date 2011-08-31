cc435bbbe8da2854ac44d25bf2a877576772f199
Marcus McConnell <marcus@bvsoftware.com>
2011-08-31 15:27:24 -0400

    Remove .CMD file that isn't needed

--- Tools/MigrationTools/migrate.cmd
+++
@@ -1,3 +0,0 @@
-.\BVSoftware.Commerce.MigrationConsole\bin\Debug\BVMigrate.exe -bv6=http://www.samplelocalhost6.com/ -imagefolder=c:\projects\AlexanderFiles -mode=5 -sqlserver=. -sqldatabase=AlexanderPublishing -sqluser=sa -sqlpassword=bobacat1618! -importproducts=Y -importcategories=N -importusers=N -importaffiliates=N -importorders=N -importothers=Y -clearproducts=N -clearcategories=N -clearorders=Y -clearaffiliates=N -usemetric=N -apikey=2-24f128dd-a492-410e-b8cb-b51adb06763c
-
-
